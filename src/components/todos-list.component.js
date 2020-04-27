4114abec350369c88c9e35d81912ddaf26207962
muirurikin <muirurikin@gmail.com>
2020-04-27 19:41:08 +0300

    Front End Commit

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+
+    onChangeTodoDescription(e) {
+        this.setState({
+            todo_description: e.target.value
+        });
+    }
+
+    onChangeTodoResponsible(e) {
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+
+    onChangeTodoPriority(e) {
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
+    
+
+    render() {
+        return (
+            <div>
+                <p>Welcome to Todos List Component!!</p>
+            </div>
+        )
+    }
+}
